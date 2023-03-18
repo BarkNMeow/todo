efb136bbb3f3d751b9359daf8c0c3296cb7c7481
Lee Junmo <leejunmo84@gmail.com>
2023-03-19 01:59:36 +0900

    Added datetime input and visualization

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,14 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 
-import { Todolist } from "./todolist";
+import { Todolist, TodoInfo } from "./todolist";
 
 import './index.css';
+import './todo.css';
 
 interface Appstate {
-    tabindex: Number,
+    todo: Array<TodoInfo>
+    tabindex: number,
 }
 
 class App extends React.Component<{}, Appstate>{
@@ -14,10 +16,20 @@ class App extends React.Component<{}, Appstate>{
         super(props)
         this.state = {
             tabindex: 0,
+            todo: []
         }
+
+        this.addTodo = this.addTodo.bind(this);
+    }
+    
+    addTodo(text: string, due: number) {
+        this.setState({
+            tabindex: this.state.tabindex,
+            todo: [...this.state.todo, {text: text, due: due}],
+        })
     }
 
-    changeTab(n: Number){
+    changeTab(n: number){
         this.setState({
             tabindex: n,
         })
@@ -26,7 +38,7 @@ class App extends React.Component<{}, Appstate>{
     renderTab(){
         switch(this.state.tabindex){
             case 0:
-                return (<Todolist />);
+                return (<Todolist todo={this.state.todo} addTodo={this.addTodo}/>);
             case 1:
                 return(<div />);
             default:
--- src/todolist.tsx
+++ src/todolist.tsx
@@ -1,76 +1,122 @@
+import { off } from "process";
 import * as React from "react";
+import { mockComponent } from "react-dom/test-utils";
+import { isReturnStatement } from "typescript";
 import './index.css';
 
-interface TodolistType {
-    add: Boolean,
-    todo: Array<TodoProp>,
+interface TodoInfo {
+    text: string,
+    due: number,
 }
 
-interface TodoProp {
-    text: String,
-    due: Number,
+interface TodolistProp {
+    todo: Array<TodoInfo>
+    addTodo: any,
 }
 
-class Todo extends React.Component <TodoProp, {}>{
-    constructor(props: TodoProp){
-        super(props);
-    }
+interface TodolistState {
+    add: boolean,
+}
 
+class Todo extends React.Component <TodoInfo, {}>{
     render() {
+        const curtime = Date.now();
+        const due = this.props.due;
+        const interval = new Date((due - curtime) > 0 ? due - curtime : 0).valueOf() / 1000;
+        
+        const time_piece = [
+            [Math.floor(interval / 86400), 'd'], 
+            [Math.floor((interval % 86400) / 3600), 'h'],
+            [Math.floor((interval % 3600) / 60), 'm'],
+            [Math.floor(interval % 60), 's']
+        ];
+
+        const timeleft_str = time_piece.filter(val => val[0] !== 0).map(val => val[0].toString() + val[1]).join(' ');
+
         return (
             <div className="todo-list">
                 <div className="todo-text">{ this.props.text }</div>
-                <div className="todo-time">4D left</div>
+                {   
+                    
+                    due === 0 ? (<div></div>) : (<div className="todo-time">{ timeleft_str } left</div>)
+                }
             </div>
         )
     }
 }
 
-class Todolist extends React.Component<{}, TodolistType>{
-    constructor(props: {}){
+class Todolist extends React.Component<TodolistProp, TodolistState>{
+    constructor(props: TodolistProp){
         super(props);
         this.state = {
-            todo: [{text: 'Do a laundary', due: 0}, {text: 'Buy some foods', due: 0}],
             add: false,
         };
-
-        console.log(this.state.add)
     }
 
     updateTodo(e: any){
         e.preventDefault();
 
-        const text = e.target.text.value;
-        this.setState({
-            todo: [...this.state.todo, {text: text, due: 0}],
-            add: false,
-        })
+        const t = e.target;
+        let flag = false;
+        if(t.text.value === ''){
+            t.text.classList.add('error');
+            flag = true;
+        }
+
+        if(t.date.value === ''){
+            t.date.classList.add('error');
+            flag = true;
+        }
+
+        if(t.time.value === ''){
+            t.time.classList.add('error');
+            flag = true;
+        }
+
+        const duetime = new Date(e.target.date.value + ' ' + e.target.time.value);
+        const due = duetime.valueOf();
+
+        if(due <= Date.now()){
+            t.date.classList.add('error');
+            t.time.classList.add('error');
+            flag = true;
+        }
+
+        if(flag) return;
+            
+        this.props.addTodo(e.target.text.value, duetime.valueOf());
+        this.setState({add: false});
     }
 
     render() {
         return(
             <div className="tab">
                 <div className="tab-title">
-                    You have {this.state.todo.length} TODOs left! 
-                    <button onClick={_ => this.setState({todo: this.state.todo, add: true})}>Add</button>
+                    {this.props.todo.length} TODOs left! 
+                    <button onClick={_ => this.setState({add: true})}>Add</button>
                 </div>
                 <div className="todo-list-scroll">
-                    {this.state.todo.map((v, i) => (<Todo key={i} text={v.text} due={v.due} />))}
+                    {this.props.todo.map((v, i) => (<Todo key={i} text={v.text} due={v.due} />))}
                     {   
-                        this.state.add == true ? (
+                        this.state.add ? (
                         <form className="todo-add" onSubmit={e => this.updateTodo(e)}>
                             <input placeholder="Add your TODO" name="text"></input>
-                            <div>
-                                <button>confirm</button>
+                            <div className="todo-add-datetime-wrapper">
+                                <input type="date" name="date"></input>
+                                <input type="time" name="time"></input>
+                            </div>
+                            <div className="todo-add-btn-wrapper">
+                                <button id="btn-todo-confirm">confirm</button>
+                                <button id="btn-todo-cancel" onClick={e => {e.stopPropagation(); this.setState({add: false})}}>cancel</button>
                             </div>
                         </form>
                         ) : (<div></div>)
                     }
                 </div>
-
             </div>
         )
     }
 }
 
 export { Todolist }
+export type {TodoInfo}
