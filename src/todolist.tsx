31d580ea057efd7b2a4a8cc02563b962f9cfb956
Lee Junmo <leejunmo84@gmail.com>
2023-03-18 23:57:49 +0900

    Added function to add the TODOs

--- src/todolist.tsx
+++ src/todolist.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import './index.css';
 
 interface TodolistType {
+    add: Boolean,
     todo: Array<TodoProp>,
 }
 
@@ -30,14 +31,43 @@ class Todolist extends React.Component<{}, TodolistType>{
         super(props);
         this.state = {
             todo: [{text: 'Do a laundary', due: 0}, {text: 'Buy some foods', due: 0}],
+            add: false,
         };
+
+        console.log(this.state.add)
+    }
+
+    updateTodo(e: any){
+        e.preventDefault();
+
+        const text = e.target.text.value;
+        this.setState({
+            todo: [...this.state.todo, {text: text, due: 0}],
+            add: false,
+        })
     }
 
     render() {
         return(
             <div className="tab">
-                <div className="tab-title">You have {this.state.todo.length} TODOs left! <button>Add</button></div>
-                {this.state.todo.map((v, i) => (<Todo key={i} text={v.text} due={v.due} />))}
+                <div className="tab-title">
+                    You have {this.state.todo.length} TODOs left! 
+                    <button onClick={_ => this.setState({todo: this.state.todo, add: true})}>Add</button>
+                </div>
+                <div className="todo-list-scroll">
+                    {this.state.todo.map((v, i) => (<Todo key={i} text={v.text} due={v.due} />))}
+                    {   
+                        this.state.add == true ? (
+                        <form className="todo-add" onSubmit={e => this.updateTodo(e)}>
+                            <input placeholder="Add your TODO" name="text"></input>
+                            <div>
+                                <button>confirm</button>
+                            </div>
+                        </form>
+                        ) : (<div></div>)
+                    }
+                </div>
+
             </div>
         )
     }
